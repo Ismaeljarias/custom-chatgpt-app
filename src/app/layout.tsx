0e9bfb644f1cbcbd16b9ffeee8e3739139beb671
ismaelarias <ismaeljarias@gmail.com>
2024-09-26 16:14:38 -0600

    Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,14 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
+import Link from "next/link";
+import { Inter } from 'next/font/google';
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "NextJS ChatGPT App",
   description: "ChatGPT brought to yoy by NextJS",
 };
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({
   children,
@@ -24,11 +16,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+
+
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.className} px-2 md:px-5`}
       >
-        {children}
+       <header className="text-white font-bold bg-green-900 text-2xl p-2 mb-3 rounded-b-lg shadow-gray-700 shadow-lg flex">
+          <div className="flex flex-grow">
+            <Link href="/">GPT Chat</Link>
+            <Link href="/about" className="ml-5 font-light">About</Link>
+          </div>
+          <div></div>
+        </header>
+        <div className="flex flex-col md:flex-row">
+          <div className="flex-grow">{children}</div>
+        </div>
       </body>
     </html>
   );
